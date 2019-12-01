1ecf445b1890b711c505e245573fd47f786e5fd9
Logan Scavo <diggerspgh@gmail.com>
2019-12-01 15:37:18 -0500

    Day 1

---
+++ day-1/main.fsx
@@ -0,0 +1,24 @@
+open System.IO
+
+let moduleFuel mass = (mass / 3) - 2
+
+let rec fuelFuel mass =
+  let fuel = moduleFuel mass
+  match fuel <= 0 with
+    | false -> fuel + (fuelFuel fuel)
+    | true -> 0
+
+
+let modules = File.ReadAllLines "input.txt" |> Seq.map int
+
+// Part 1
+modules
+  |> Seq.map moduleFuel
+  |> Seq.sum
+  |> printfn "The total fuel requirement is: %i"
+
+// Part 2
+modules
+  |> Seq.map fuelFuel
+  |> Seq.sum
+  |> printfn "The total (for real this time!) fuel requirement is: %i"
