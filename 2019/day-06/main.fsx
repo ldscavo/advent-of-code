ce099a158580699287a92aa6ec579b1862746abf
Logan Scavo <logan@scavo.dev>
2020-08-12 17:13:04 -0400

    Day 6 part 2

--- 2019/day-06/main.fsx
+++ 2019/day-06/main.fsx
@@ -8,8 +8,8 @@ let toOrbit (line: array<string>): Orbit =
 let rec getOrbitCount count (orbits: seq<Orbit>) (orbit: Orbit) =
   Seq.tryFind (fun o -> (fst orbit) = (snd o)) orbits
   |> function
-     | Some orb -> getOrbitCount (count + 1) orbits orb
-     | None -> count
+   | Some orb -> getOrbitCount (count + 1) orbits orb
+   | None -> count
 
 let orbits =
   File.ReadAllLines "input.txt"
@@ -18,4 +18,26 @@ let orbits =
 // Part 1
 orbits
 |> Seq.sumBy (getOrbitCount 1 orbits)
+|> printfn "%A"
+
+//Part 2
+let rec getOrbitSteps (steps: Set<string>) (orbits: seq<Orbit>) (orbit: Orbit) =
+  Seq.tryFind (fun o -> (fst orbit) = (snd o)) orbits
+  |> function
+   | Some orb -> getOrbitSteps (Set.add (fst orb) steps) orbits orb
+   | None -> steps
+
+let getSteps name =
+  Seq.find (fun o -> (snd o) = name) orbits
+  |> (fun orb -> getOrbitSteps (Set.ofList [(fst orb)]) orbits orb)
+
+let yourSteps = getSteps "YOU"
+let santasSteps = getSteps "SAN"
+
+let sharedSteps = Set.intersect yourSteps santasSteps
+
+let distinctSteps = 
+  Set.union (Set.difference yourSteps sharedSteps) (Set.difference santasSteps sharedSteps)
+
+Set.count distinctSteps
 |> printfn "%A"
