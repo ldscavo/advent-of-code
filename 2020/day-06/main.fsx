9a0663164efe3cd1aa71075a3d90e9a070f50c3d
ldscavo <logan@scavo.dev>
2021-11-05 22:07:55 -0400

    day 6 part 2

--- 2020/day-06/main.fsx
+++ 2020/day-06/main.fsx
@@ -10,3 +10,12 @@ File.ReadAllText "input.txt"
       >> List.distinct
       >> List.length )
 |> (printfn "The sum of the counts is: %i")
+
+// Part 2
+File.ReadAllText "input.txt"
+|> (fun s -> s.Split "\r\n\r\n")
+|> Array.map (fun s ->
+    s.Split "\r\n" |> Array.map (Seq.toList >> Set.ofList)
+    |> (fun ss -> ss |> Array.fold Set.intersect (Array.head ss)))
+|> Array.sumBy Set.count
+|> printfn "The sum of the unique answers is: %i"
