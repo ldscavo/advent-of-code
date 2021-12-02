9efe4f2d32a73a9b5c0ca71c42212d54a8bfd7ad
ldscavo <logan@scavo.dev>
2021-12-02 12:08:45 -0500

    This can be one step

--- 2021/day-01/main.fsx
+++ 2021/day-01/main.fsx
@@ -7,8 +7,7 @@ let depths =
 // Part 1
 depths
 |> Seq.pairwise
-|> Seq.map (fun (d1, d2) -> d2 > d1)
-|> Seq.filter id
+|> Seq.filter (fun (d1, d2) -> d2 > d1)
 |> Seq.length
 |> printfn "There are %i increasing measurements"
 
@@ -17,7 +16,6 @@ depths
 |> Seq.windowed 3
 |> Seq.map Array.sum
 |> Seq.pairwise
-|> Seq.map (fun (sum1, sum2) -> sum2 > sum1)
-|> Seq.filter id
+|> Seq.filter (fun (sum1, sum2) -> sum2 > sum1)
 |> Seq.length
 |> printfn "There are %i increasing measurements"
